27cce302028372863076dad8a35453598ea75a28
maria2580 <marin6670@gmail.com>
2022-08-25 11:57:55 +0900

    .

--- app/src/main/java/com/example/today_menu_app/network/RetrofitClient.java
+++ app/src/main/java/com/example/today_menu_app/network/RetrofitClient.java
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://43.200.172.82:8080/";
+    private static final String BASE_URL = URL_holder.getURL();
     public static Retrofit getInstance(){
         Gson gson = new GsonBuilder().setLenient().create();
         return new Retrofit.Builder()
